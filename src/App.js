701570721e67feae78d4ed5f0a24cbb87e01a83b
Arthur <arthur91021@gmail.com>
2024-12-04 13:49:00 -0800

    messages added

--- src/App.js
+++ src/App.js
@@ -111,6 +111,13 @@ function Item({ item, onDeleteItem, onToggleItem }) {
 }
 
 function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list! 🚀</em>
+      </p>
+    );
+
   const numItems = items.length;
   const numPacked = items.filter((item) => item.packed).length;
   const percentage = Math.round((numPacked / numItems) * 100);
@@ -118,8 +125,10 @@ function Stats({ items }) {
   return (
     <footer className="stats">
       <em>
-        💼 You have {numItems} items on your list, and you already packed{" "}
-        {numPacked} ({percentage}%)
+        {percentage === 100
+          ? "You got everything! Ready to go! ✈"
+          : `💼 You have ${numItems} items on your list, and you already packed
+        ${numPacked} (${percentage}%)`}
       </em>
     </footer>
   );
